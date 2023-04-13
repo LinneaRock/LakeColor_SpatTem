9021ff4eb49d3f915b7ad63a45d3e8ad57037cc7
LinneaRock <lrock@wisc.edu>
2023-04-13 15:33:48 -0600

    updating temporal restrictions

--- Code/temporal_restriction.R
+++ Code/temporal_restriction.R
@@ -1,3 +1,13 @@
 temporal <- masterDF |>
   mutate(date=as.Date(date)) |>
-  filter(between(doy, 152, 258))
+  filter(between(doy, 152, 258)) |>
+  group_by(lagoslakeid, year) |>
+  add_count() |>
+  filter(n >= 3) |>
+  ungroup()
+
+
+temporal_med <- temporal |>
+  group_by(lagoslakeid, year) |>
+  summarise(ann_dWL = median(dWL)) |>
+  ungroup()
