8d9fc272ed474def5487969c2866f482bf6250e7
LinneaRock <lrock@wisc.edu>
2023-04-14 11:18:26 -0600

    CART model

--- Code/cartTest.R
+++ Code/cartTest.R
@@ -1,40 +1,67 @@
+# Script to run CART analysis on trend data
+
 source("Data/Call_data.R")
+source('Code/temporal_restriction.R')
+dwl_all <- read.csv('Data/dWL_all.csv')
 
 
+library(tidyverse)
+library(rpart)
+library(rsample)
+library(caret)
+library(rpart.plot)
+library(yardstick)
 
 set.seed(42)
 
-cart <- temporal %>% 
-  filter()
+# Format dataset for CART analysis ####
+cart_data <- dWL_all |>
+  select(lagoslakeid, Trend) |>
+  distinct() |>
+  left_join(temporal |>
+  select(lagoslakeid, lake_waterarea_ha, lake_connectivity_class,lake_totalperimeter_m, lake_elevation_m, ws_area_ha, ws_lake_arearatio) |>  ungroup() |> select(-year) |> distinct()) |>
+  rename(`Lake area (ha)` = lake_waterarea_ha,
+         Connectivity = lake_connectivity_class,
+         `Perimeter (m)` = lake_totalperimeter_m,
+         Elevation = lake_elevation_m,
+         `Residence Time` = ws_lake_arearatio) |>
+  ungroup() |>
+  select(-lagoslakeid) |>
+  drop_na() |>
+  mutate(Trend.new = case_when(Trend %in% c('Blue -> Greener', 'Intensifying Green/brown') ~ 'Greening',
+                                Trend %in% c('Green -> Bluer', 'Intensifying Blue') ~ 'Blueing',
+                                TRUE ~ 'No trend')) |>
+  select(-Trend) |>
+  mutate(Trend.new = as.factor(Trend.new))
 
-train_d <- temporal %>%
-  sample_frac(0.8) %>%
-   mutate_if(is.numeric, round, digits=0) #%>%
-  # rename("MAAT °C"=air_temp,
-  #        "mean depth"=meandepth,
-  #        "max. depth"=maxdepth,
-  #        "watershed slope"=slope,
-  #        WSA=ws_area)
+cart_data |> count(Trend.new)
 
-#how many lakes in the training dataset?
-# length(unique(train_d$Hylak_id))
-test_d <- temporal %>%
-  dplyr::filter(!lagoslakeid %in% train_d$lagoslakeid) %>%
-  dplyr::select(-lagoslakeid,-dWL) %>%
-  mutate_if(is.numeric, round, digits=0)#%>%
-  # rename("MAAT °C"=air_temp,
-  #        "mean depth"=meandepth,
-  #        "max. depth"=maxdepth,
-  #        "watershed slope"=slope,
-  #        WSA=ws_area)
-  
-train_d <- train_d %>%
-  dplyr::select(-lagoslakeid,-dWL) 
 
-prune_mod <-caret::train(
-    group ~., data = train_d, method = "rpart",
-    trControl = trainControl("boot", number = 100),
-    tuneLength = 100
-    )
+# Training data ####
+# split training and testing data by 60/40% 
+test_inds <- initial_split(cart_data, 0.6)
+# Split data into test/train 
+train <- training(test_inds) 
+test <- testing(test_inds) 
 
-prune_mod
+
+
+# Run CART model ####
+CART_mod <- rpart(Trend.new~., data=train, method='class', cp=0.01)
+
+## test cart model ####
+test$predict <- predict(CART_mod, test, 'class')
+cm <- conf_mat(test, Trend.new, predict)
+accuracy(test, Trend.new, predict)
+cm
+
+## get the best cp ####
+printcp(CART_mod)
+bestcp <- CART_mod$cptable[which.min(CART_mod$cptable[,'xerror']), 'CP']
+
+CART_mod_pruned <- prune(CART_mod, cp=bestcp)
+
+## print tree ####
+plot(CART_mod_pruned)
+text(CART_mod_pruned, cex=0.8, use.n=TRUE, xpd=TRUE)
+
