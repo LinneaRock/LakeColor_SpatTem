83c8ffa5a65ffd53ceed2572a2074f245c3dfb31
Spencer Cruz <spencercruz24@gmail.com>
2023-04-14 14:47:19 -0600

    UN COMMENTED EVERYTHING

--- Code/Code for interactive maps.R
+++ Code/Code for interactive maps.R
@@ -1,6 +1,6 @@
 #Interactive maps for lake color
 
-# masterDF <- read.csv('Data/masterDF.csv')
+masterDF <- read.csv('Data/masterDF.csv')
 
 
 
@@ -16,44 +16,44 @@ library(leafpop)
 library(RColorBrewer)
 
 
-# #2021 ID, MT, WY, CO
-# firstfour<-masterDF[masterDF$lake_centroidstate==c("WY", "CO", "MT", "ID"),]
-# firstfour2021<-firstfour[firstfour$year=="2021",]
-# 
-# 
-# 
-# mapview(firstfour2021, 
-#         zcol = "dWL", 
-#         xcol = "lake_lon_decdeg", 
-#         ycol = "lake_lat_decdeg", 
-#         crs = 4269, 
-#         grid = FALSE)
+#2021 ID, MT, WY, CO
+firstfour<-masterDF[masterDF$lake_centroidstate==c("WY", "CO", "MT", "ID"),]
+firstfour2021<-firstfour[firstfour$year=="2021",]
 
 
-#1991 ID, MT, WY, CO
+
+mapview(firstfour2021,
+        zcol = "dWL",
+        xcol = "lake_lon_decdeg",
+        ycol = "lake_lat_decdeg",
+        crs = 4269,
+        grid = FALSE)
 
 
-# firstfour1991<-firstfour[firstfour$year=="1991",]
-# 
-# 
-# 
-# mapview(firstfour1991, 
-#         zcol = "dWL", 
-#         xcol = "lake_lon_decdeg", 
-#         ycol = "lake_lat_decdeg", 
-#         crs = 4269, 
-#         grid = FALSE)
+# 1991 ID, MT, WY, CO
 
 
-#2021 all west states
+firstfour1991<-firstfour[firstfour$year=="1991",]
 
 
-# masterdf2021<-masterDF[masterDF$year=="2021",]
+
+mapview(firstfour1991,
+        zcol = "dWL",
+        xcol = "lake_lon_decdeg",
+        ycol = "lake_lat_decdeg",
+        crs = 4269,
+        grid = FALSE)
 
 
-# mapview(masterdf2021, 
-#         zcol = "dWL", 
-#         xcol = "lake_lon_decdeg", 
-#         ycol = "lake_lat_decdeg", 
-#         crs = 4269, 
-#         grid = FALSE)
+# 2021 all west states
+
+
+masterdf2021<-masterDF[masterDF$year=="2021",]
+
+
+mapview(masterdf2021,
+        zcol = "dWL",
+        xcol = "lake_lon_decdeg",
+        ycol = "lake_lat_decdeg",
+        crs = 4269,
+        grid = FALSE)
