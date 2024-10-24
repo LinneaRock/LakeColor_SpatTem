c8e698a094092a61e8b36f574bc23268f5622f7e
Linnea Ann Rock <lrock1@uwyo.edu>
2024-10-24 16:11:17 -0600

    more plots of ecoregions for slopes fig

--- Code/Ecoregions_maps/plottingregions.R
+++ Code/Ecoregions_maps/plottingregions.R
@@ -19,7 +19,7 @@ regions.sf <- read_sf("Data/aggr_ecoregions_2015/Aggr_Ecoregions_2015.shp")|>
   # fix this dumbass typo
   mutate(WSA9_NAME = ifelse(WSA9_NAME == "Temporate Plains", "Temperate Plains", WSA9_NAME))
 
-
+# plot with custom colors
 ggplot() +
   geom_sf(regions.sf |> select(-WSA9), mapping=aes(),fill='white', color='grey30') +
   geom_sf(regions.sf, mapping=aes(fill=WSA9),color='black') +
@@ -40,3 +40,24 @@ ggplot() +
   facet_wrap(~WSA9)
 
 ggsave('Code/Ecoregions_maps/facetedregions.png', height = 4.5, width = 6.5, units = "in", dpi = 1200)
+
+# plot without custom colors
+ggplot() +
+  geom_sf(regions.sf |> select(-WSA9), mapping=aes(),fill='white', color='grey30') +
+  geom_sf(regions.sf, mapping=aes(fill=WSA9),fill='black') +
+  theme_minimal() +
+  labs(x = "", y = "") +
+  theme(plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 0, family = "serif"),
+        legend.position = 'none',
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  facet_wrap(~WSA9)
+
+ggsave('Code/Ecoregions_maps/facetedregions_noColors.png', height = 4.5, width = 6.5, units = "in", dpi = 1200)
